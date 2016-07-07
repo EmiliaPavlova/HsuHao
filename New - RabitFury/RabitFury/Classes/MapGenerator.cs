d4c2a78aa29b01d1998436cfe9dddf181f8037ed
Georgi Karaboichev <djorkaevhell@abv.bg>
2016-07-08 02:57:33 +0300

    Extending Map

--- New - RabitFury/RabitFury/Classes/MapGenerator.cs
+++ New - RabitFury/RabitFury/Classes/MapGenerator.cs
@@ -14,9 +14,9 @@
             List<Platform> tempList = new List<Platform>();
 
             Random random = new Random();
-            for (int i = 0; i < 41; i++)
+            for (int i = 0; i < 60; i++)
             {
-                if(i > 5 && (i == 9 || i == 15 || i == 17 || i == 23 || i == 29))
+                if(i > 5 && (i == 9 || i == 15 || i == 17 || i == 23 || i == 29 || i == 33 || i == 35 || i == 39 || i == 45))
                 {
                     tempList.Add(new Platform(new Vector2(0.11f * i, 0.54f), new Vector2(0.11f, 0.11f), Color.White, theTexture[2], PlatformType.Dirt));
                     tempList.Add(new Platform(new Vector2(0.11f * i, 0.65f), new Vector2(0.11f, 0.11f), Color.White, theTexture[8], PlatformType.Dirt));
@@ -37,25 +37,28 @@
                     tempList.Add(new Platform(new Vector2(0.11f * i, 0.54f), new Vector2(0.11f, 0.11f), Color.White, theTexture[1], PlatformType.Dirt));
                     tempList.Add(new Platform(new Vector2(0.11f * i, 0.65f), new Vector2(0.11f, 0.11f), Color.White, theTexture[8], PlatformType.Dirt));
                 }
-                if(i == 38)
+                if(i == 54)
                 {
                     tempList.Add(new Platform(new Vector2((0.11f * i) + 0.25f, 0.22f), new Vector2(1.1f, 1.1f), Color.White, null, PlatformType.Dirt));
                 }
             }
 
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 35; i++)
             {
-                if (i == 4 || i == 10 || i == 12 || i == 16)
+                if (i == 4 || i == 10 || i == 12 || i == 16 || i == 18 || i == 20 || i == 23 || i == 27 )
                 {
-                    if(i == 4 || i == 16)
+                    if(i == 4 || i == 16 || i == 18 || i == 20 || i == 28)
                     {
                         tempList.Add(new Platform(new Vector2(0.195f * i + 0.26f, 0.1f), new Vector2(0.065f, 0.065f), Color.White, theTexture[12], PlatformType.Dirt));
                         tempList.Add(new Platform(new Vector2((0.195f * i) + 0.065f + 0.26f, 0.1f), new Vector2(0.065f, 0.065f), Color.White, theTexture[13], PlatformType.Dirt));
                         tempList.Add(new Platform(new Vector2((0.195f * i) + 0.13f + 0.26f, 0.1f), new Vector2(0.065f, 0.065f), Color.White, theTexture[14], PlatformType.Dirt));
                     }
-                    tempList.Add(new Platform(new Vector2(0.195f * i, 0.3f), new Vector2(0.065f, 0.065f), Color.White, theTexture[12], PlatformType.Dirt));
-                    tempList.Add(new Platform(new Vector2((0.195f * i) + 0.065f, 0.3f), new Vector2(0.065f, 0.065f), Color.White, theTexture[13], PlatformType.Dirt));
-                    tempList.Add(new Platform(new Vector2((0.195f * i) + 0.13f, 0.3f), new Vector2(0.065f, 0.065f), Color.White, theTexture[14], PlatformType.Dirt));
+                    if (i != 18 && i != 20 && i != 28)
+                    {
+                        tempList.Add(new Platform(new Vector2(0.195f * i, 0.3f), new Vector2(0.065f, 0.065f), Color.White, theTexture[12], PlatformType.Dirt));
+                        tempList.Add(new Platform(new Vector2((0.195f * i) + 0.065f, 0.3f), new Vector2(0.065f, 0.065f), Color.White, theTexture[13], PlatformType.Dirt));
+                        tempList.Add(new Platform(new Vector2((0.195f * i) + 0.13f, 0.3f), new Vector2(0.065f, 0.065f), Color.White, theTexture[14], PlatformType.Dirt));
+                    }
                 }
             }
             tempList.Add(new Platform(new Vector2(0.07f , 0.22f), new Vector2(0.71f, 0.71f), Color.White, null, PlatformType.Dirt));
