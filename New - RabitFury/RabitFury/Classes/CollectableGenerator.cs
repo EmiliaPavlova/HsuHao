4b139bd66a907deb38755b09dc9b5886b75a3bba
Emilia Pavlova <emilia.pavlova@gmail.com>
2016-07-09 20:29:03 +0300

    Added collectables

--- New - RabitFury/RabitFury/Classes/CollectableGenerator.cs
+++ New - RabitFury/RabitFury/Classes/CollectableGenerator.cs
@@ -16,8 +16,28 @@
 
             //TODO: Check positions with Platform
 
-            collectables.Add(new Collectable(new Vector2(0.07f, 0.22f), new Vector2(0.71f, 0.71f), Color.White, null));
-            collectables.Add(new Collectable(new Vector2(0.00f, 0.75f), new Vector2(100f, 0.1f), Color.White, null));
+            //collectables.Add(new Collectable(new Vector2(0.8f, 0.465f), new Vector2(0.0335f, 0.05f), Color.White, theTexture[0]));
+            //collectables.Add(new Collectable(new Vector2(1f, 0.47f), new Vector2(0.04f, 0.04f), Color.White, theTexture[1]));
+
+            for (int j = 1; j < 100; j++)
+            {
+                float i = random.Next(1, 100) * j * 0.04f;
+                //if (i > 5.0 &&
+                //    (i == 9 || i == 15 || i == 17 || i == 23 || i == 29 || i == 33 || i == 35 || i == 39 || i == 45))
+                //{
+                //    continue;
+                //}
+                //else
+                //{
+                    collectables.Add(new Collectable(new Vector2(i, 0.465f), new Vector2(0.0335f, 0.05f), Color.White, theTexture[0]));
+                //}
+            }
+
+            for (int j = 1; j < 100; j++)
+            {
+                float i = random.Next(1, 100) * j * 0.06f;
+                collectables.Add(new Collectable(new Vector2(i, 0.47f), new Vector2(0.04f, 0.04f), Color.White, theTexture[1]));
+            }
 
             return collectables;
         }
